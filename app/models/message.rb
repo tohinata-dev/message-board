2570df68c2ea721764ed2b718f51afe23a49b942
shoheiyamaguchi <tohinata.dev@gmail.com>
2016-06-22 14:08:23 +0000

    Implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ActiveRecord::Base
+    
+    validates :name ,length:{maximum:20},presence:true
+    
+    validates :name , length: {minimum:2, maximum:30} , presence: true
+    
 end
