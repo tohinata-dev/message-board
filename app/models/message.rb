62141138c15fbc7af0a3594dd1cd78773fff4aa0
shoheiyamaguchi <tohinata.dev@gmail.com>
2016-06-28 11:07:29 +0000

    kadai2 end

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,10 @@
 class Message < ActiveRecord::Base
     
-    validates :name ,length:{maximum:20},presence:true
+    validates :name ,length:{maximum:20},presence: true
     
-    validates :name , length: {minimum:2, maximum:30} , presence: true
+    validates :body ,length: {minimum:2, maximum:30} , presence: true
     
+    validates :age  ,numericality: {
+        only_integer: true, greater_than_or_equal_to: 0
+    }
 end
